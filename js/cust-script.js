b706916836be48d6130ab5d9879c281a1d1dd999
richmartins <richard.martinstenorio@epfl.ch>
2018-05-24 16:23:37 +0200

    header changed

--- js/cust-script.js
+++ js/cust-script.js
@@ -1,78 +1,90 @@
 window.onscroll = function() {
-  scrolly()
+    scrolly()
 };
 
 function scrolly() {
-  if (document.documentElement.scrollTop > wrapper.clientHeight || document.body.scrollTop > wrapper.clientHeight) {
-    document.getElementById("scroll-menu").className = "Fixed"
-  } else {
-    document.getElementById("scroll-menu").className = ""
-  }
+    if (document.documentElement.scrollTop > wrapper.clientHeight || document.body.scrollTop > wrapper.clientHeight) {
+        document.getElementById("scroll-menu").className = "Fixed"
+    } else {
+        document.getElementById("scroll-menu").className = ""
+    }
 }
 $(document).ready(function() {
-  $("h4").each(function() {
-    $elf = $(this);
-    $("#scroll-ul").append($("<li>").append($("<a>", {
-      href: "#" + $elf.attr("id")
-    }).text($elf.text())))
-  });
-  var a = $(this);
-  if (a.width() < 770) {
-    $("#menu").hide()
-  }
-  $(window).on("resize", function() {
-    if (a.width() <= 770) {
-      $("#menu").hide()
-    } else {
-      $("#menu").show()
-    }
-  });
-  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(b) {
-    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-      var c = $(this.hash);
-      c = c.length ? c : $("[name=" + this.hash.slice(1) + "]");
-      if (c.length) {
-        b.preventDefault();
-        $("html, body").animate({
-          scrollTop: c.offset().top
-        }, 1000, function() {
-          var d = $(c);
-          d.focus();
-          if (d.is(":focus")) {
-            return false
-          } else {
-            d.attr("tabindex", "-1");
-            d.focus()
-          }
+    $(window).scroll(function() {
+        $(".hidded").each(function(a) {
+            var c = $(this).offset().top + $(this).outerHeight();
+            var d = $(window).scrollTop() + $(window).height();
+            if (d > c) {
+                $(this).animate({
+                    opacity: "1"
+                }, 250)
+            }
         })
-      }
+    });
+    $("h4").each(function() {
+        $elf = $(this);
+        $("#scroll-ul").append($("<li>").append($("<a>", {
+            href: "#" + $elf.attr("id")
+        }).text($elf.text())))
+    });
+    var b = $(this);
+    if (b.width() < 770) {
+        $("#menu").hide()
     }
-  });
-  $.fn.extend({
-    animateCss: function(c, d) {
-      var b = (function(f) {
-        var g = {
-          animation: "animationend",
-          OAnimation: "oAnimationEnd",
-          MozAnimation: "mozAnimationEnd",
-          WebkitAnimation: "webkitAnimationEnd",
-        };
-        for (var e in g) {
-          if (f.style[e] !== undefined) {
-            return g[e]
-          }
+
+    $(window).on("resize", function() {
+        if (b.width() <= 770) {
+            $("#menu").hide()
+        } else {
+            $("#menu").show()
         }
-      })(document.createElement("div"));
-      this.addClass("animated " + c).one(b, function() {
-        $(this).removeClass("animated " + c);
-        if (typeof d === "function") {
-          d()
+    });
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(a) {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var d = $(this.hash);
+            d = d.length ? d : $("[name=" + this.hash.slice(1) + "]");
+            if (d.length) {
+                a.preventDefault();
+                $("html, body").animate({
+                    scrollTop: d.offset().top
+                }, 1000, function() {
+                    var c = $(d);
+                    c.focus();
+                    if (c.is(":focus")) {
+                        return false
+                    } else {
+                        c.attr("tabindex", "-1");
+                        c.focus()
+                    }
+                })
+            }
         }
-      });
-      return this
-    },
-  });
-  $("#wrapper-content").animateCss("bounceInUp", function() {
-    $("#arrow-down").animateCss("bounce")
-  })
+    });
+    $.fn.extend({
+        animateCss: function(f, e) {
+            var a = (function(d) {
+                var c = {
+                    animation: "animationend",
+                    OAnimation: "oAnimationEnd",
+                    MozAnimation: "mozAnimationEnd",
+                    WebkitAnimation: "webkitAnimationEnd",
+                };
+                for (var h in c) {
+                    if (d.style[h] !== undefined) {
+                        return c[h]
+                    }
+                }
+            })(document.createElement("div"));
+            this.addClass("animated " + f).one(a, function() {
+                $(this).removeClass("animated " + f);
+                if (typeof e === "function") {
+                    e()
+                }
+            });
+            return this
+        },
+    });
+    $("#wrapper-content").animateCss("bounceInUp", function() {
+        $("#arrow-down").animateCss("bounce")
+    })
 });
