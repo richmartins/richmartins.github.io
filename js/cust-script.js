3d278d278a78eb722ef6f8b29b5e007a27ef2f61
richmartins <richard.martinstenorio@gmail.com>
2020-01-07 16:00:02 +0100

    [DONE] v2

---
+++ js/cust-script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('a[href*=\\#]').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+    });
+});
+
+
+var app = document.getElementById('typewriteText');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello World !')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Welcome to another github page')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Happy geek')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Love to code')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Eager to learn more')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Probably in the middle of a CTF')
+    .pauseFor(1500)
+    .start();
