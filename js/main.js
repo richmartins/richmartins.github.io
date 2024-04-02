e1efd9fcc9e531c4e1bdd6a9d32bfe28ca14f8e0
richmartins <richard.tenorio@excellgene.com>
2024-04-02 16:49:26 +0200

    changed all design

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-$(document).ready(function () {
-    $('a[href*=\\#]').on('click', function (event) {
-        event.preventDefault();
-        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
-    });
-});
+document.querySelectorAll('.row .col').forEach((row) => {
+    if (row.parentNode.id !== 'header') { 
+        let prev_row_id = row.parentNode.previousElementSibling.id;
+        let html_arrow_up = document.createElement('div');
+        html_arrow_up.className = 'arrow-container bounce animate';
+        html_arrow_up.innerHTML = `<a href="#${prev_row_id}"><p><i class="up"></i><br><i class="up"></i></p></a>`;
+        row.insertBefore(html_arrow_up, row.firstChild);
+    }
 
+    if (row.parentNode.nextElementSibling !== null) { 
+        let next_row_id = row.parentNode.nextElementSibling.id;
+        let html_arrow_down = document.createElement('div');
+        html_arrow_down.className = 'arrow-container bounce animated';
+        html_arrow_down.innerHTML = `<a href="#${next_row_id}"><p><i class="down"></i><br /><i class="down"></i></p></a>`;
+        row.appendChild(html_arrow_down);
+    }
+})
 
-var app = document.getElementById('typewriteText');
-
-var typewriter = new Typewriter(app, {
+let typewriter = new Typewriter(document.querySelector('#typewriteText'), {
     loop: true
 });
 
 typewriter.typeString('Hello, World !')
     .pauseFor(1500)
     .deleteAll()
-    .typeString('Presentation web site')
+    .typeString('Software Engineer')
     .pauseFor(1500)
     .deleteAll()
     .typeString('Happy geek')
     .pauseFor(1500)
     .deleteAll()
-    .typeString('Love to code')
+    .typeString('Sys Admin')
     .pauseFor(1500)
     .deleteAll()
-    .typeString('Eager to learn more')
+    .typeString('DevOps Engineer')
     .pauseFor(1500)
     .deleteAll()
-    .typeString('Probably in the middle of a CTF')
+    .typeString('CTF Player')
     .pauseFor(1500)
     .start();
